cd07372877508dcfc62c6648c5131d0ecebb04da
VosVeinard <reinardbeekman@gmail.com>
2014-05-27 16:14:21 +0200

    2.c

---
+++ session5/exercise2cT2.sql
@@ -0,0 +1,8 @@
+﻿BEGIN REPEATABLE READ
+--read2(x):  
+SELECT titel FROM Boek WHERE isbn = ' 0136067018';
+--write2(x): 
+UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = ' 0136067018';
+
+END
+
