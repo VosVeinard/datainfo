d29a5afe85b723faeea44cfde8f187d22bb56430
VosVeinard <reinardbeekman@gmail.com>
2014-05-27 14:11:39 +0200

    1.c

--- session5/exercise1b.sql
+++ session5/exercise1b.sql
@@ -1,3 +1,4 @@
+﻿INSERT INTO Boek (isbn, titel, auteur)
+VALUES 	(1234567, 'Test1 boek', 'Niels');
 INSERT INTO Boek (isbn, titel, auteur)
-VALUES 	(1234567, 'Test1 boek', 'Niels'),
-		(1234567, 'Test boek 2', 'Jeroen');
+VALUES	(1234567, 'Test boek 2', 'Jeroen');
---
+++ session5/exercise1c.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO Exemplaar (isbn, volgnummer, gewicht, kast)
+VALUES 	(1234567, 1, 1, 1);
+INSERT INTO Exemplaar (isbn, volgnummer, gewicht, kast)
+VALUES	(1234568, 2, 2, 2);
