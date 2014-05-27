1ceb7d496059518a654de1b8456443bae0c3aad0
VosVeinard <reinardbeekman@gmail.com>
2014-05-27 15:43:22 +0200

    1.g

---
+++ session5/exercise1g.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE Bestelling(
+	isbn int,
+	aantal int,
+	PRIMARY KEY (isbn),
+	FOREIGN KEY (isbn) REFERENCES Boek(isbn) ON DELETE cascade ON UPDATE cascade
+);
+
+CREATE  FUNCTION addRow() 
+RETURNS TRIGGER
+AS $$ BEGIN
+	IF((SELECT COUNT(auteur) FROM boek WHERE NEW.auteur = boek.auteur) = 1) THEN
+	INSERT INTO Bestelling (isbn, aantal) VALUES (NEW.isbn, 10);
+	END IF;
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TRIGGER addRows AFTER INSERT ON boek
+FOR EACH ROW EXECUTE PROCEDURE addRow();
